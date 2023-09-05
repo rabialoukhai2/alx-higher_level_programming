ea932d5175b1ab2f5afeb492b04b640eb917c6f9
rabialoukhai2 <rloukhi@gmail.com>
2023-09-04 17:00:37 -0700

    commit task

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[str.find("object-oriented programming"):].split(' ', 1)[0])
+print(str.split(' ', 6)[-1].split(' ', 1)[1])
