de521f071f2106f63d749dbfb2d6d19dfac5c5aa
rabialoukhai2 <rloukhi@gmail.com>
2023-09-05 16:04:07 -0700

    commit task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for c in range(ord('a'), ord('z') + 1):
-    if c != ord('e') and c != ord('q'):
+    if c != chr('e') and c != chr('q'):
         print("{:c}".format(c), end="")
