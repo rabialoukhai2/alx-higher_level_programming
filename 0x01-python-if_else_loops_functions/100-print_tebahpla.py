bf2f4de371a56564b5050e03901fa9404f842ed1
rabialoukhai2 <rloukhi@gmail.com>
2023-09-05 17:39:01 -0700

    commit task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-# Author -Bamidele Adefolaju
+# Author - rabia Loukhai
 
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+for i in range(ord('z'), ord('a') - 1, -1):
+    if i % 2 == 0:
+        print(chr(i).upper(), end='')
+    else:
+        print(chr(i).lower(), end='')
