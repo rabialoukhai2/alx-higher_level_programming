04ff01b9183af04fae6653f4ce0f309b870a9fca
rabialoukhai2 <134720091+rabialoukhai2@users.noreply.github.com>
2024-01-05 13:16:30 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Defines a peak-finding algorithm."""
 
-
 def find_peak(list_of_integers):
     """finds a peak in a list of unsorted integers"""
     if (not list_of_integers):
