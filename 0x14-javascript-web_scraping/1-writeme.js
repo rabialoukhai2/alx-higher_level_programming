4f3f1f01e12dd53d628bfac43ae4aeae39077580
rabialoukhai2 <134720091+rabialoukhai2@users.noreply.github.com>
2024-01-30 11:37:48 +0100

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,13 +4,14 @@ const fs = require('fs');
 
 // Check if the correct number of arguments is provided
 if (process.argv.length !== 4) {
-  console.error('Usage: ./script.js <file-path> <content-to-write>');
+  console.error('Usage: ./1-writeme.js <file-path> <content-to-write>');
   process.exit(1); // Exit with a non-zero status code to indicate an error
 }
 
 const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
 
+// Writing the string to the file in utf-8 encoding
 fs.writeFile(filePath, contentToWrite, 'utf-8', (error) => {
   if (error) {
     console.error(`Error writing to ${filePath}:`, error.message);
